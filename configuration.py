36b98cc3eab7ca776d7a2176e50c401653ecac8d
victorromeo <victorromeo@github.com>
2019-12-07 00:30:14 +1100

    Added base configuration

---
+++ configuration.py
@@ -0,0 +1,11 @@
+class configuration:
+    am_swdio_pin = 20
+    am_swclk_pin = 21
+    am_swo_pin = 22
+    am_rst_pin = 23
+    pir_pins = {24, 25}
+    am_mount_path = '/media/pi/Moth'
+    local_audio_path = '/home/pi/Documents/AudioMoth-Sync/audio'
+    local_visual_path = '/home/pi/Documents/AudioMoth-Sync/visual'
+    motion_sec = 5
+    motion_queue_len = 50
