9066fd18dfbcf23ce1942f0b4f66250a97dd3ff3
victorromeo <victorromeo@github.com>
2019-12-11 20:17:21 +1100

    correction to launch path

--- launch-pir.sh
+++ launch-pir.sh
@@ -3,6 +3,6 @@
 # navigate to the home directory, then to this directory, then execute python script, then back again
 
 cd /
-cd /home/pi/Documents
-sudo python3 pir.py
+cd /home/pi/Documents/AudioMoth-Sync
+sudo python3 mothPi.py
 cd /
